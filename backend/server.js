017823fc194effccee7fc2dbc2ae1b8912a08b8d
Bijoy Anil <86096110+bijoy7034@users.noreply.github.com>
2023-05-09 00:43:56 +0530

    deploy update

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = require('./routes/todoRoutes');
 const { default: mongoose } = require('mongoose');
+const cors = require('cors')
 const uri = "mongodb+srv://bijoyanil74:m8LEHXdB3CcOSnfc@cluster0.hh1jxj4.mongodb.net/?retryWrites=true&w=majority";
 const app = express()
+require('dotenv').config
+
 const PORT = process.env.PORT || 8080
 
 //Middlewares
 app.use(express.json())
+app.use(cors())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
+})
+app.get('/', (req,res)=>{
+  res.setHeader("Access-Control-Allow-Credentials","true")
 })
 app.use('/api/todo', router)
 
